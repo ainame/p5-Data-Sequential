aaa6ade94f835fdbc421f6b9b46fd038055cb7cd
ainame <s.namai.2012@gmail.com>
2014-01-17 08:54:37 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Data::Sequential - It's new $module
+
+# SYNOPSIS
+
+    use Data::Sequential;
+
+# DESCRIPTION
+
+Data::Sequential is ...
+
+# LICENSE
+
+Copyright (C) ainame.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+ainame <s.namai.2012@gmail.com>
---
+++ lib/Data/Sequential.pm
@@ -0,0 +1,39 @@
+package Data::Sequential;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Data::Sequential - It's new $module
+
+=head1 SYNOPSIS
+
+    use Data::Sequential;
+
+=head1 DESCRIPTION
+
+Data::Sequential is ...
+
+=head1 LICENSE
+
+Copyright (C) ainame.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+ainame E<lt>s.namai.2012@gmail.comE<gt>
+
+=cut
+
