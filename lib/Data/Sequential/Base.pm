e1d0ba5175e1a3e69a22d1c3cd8ba57d81042a87
ainame <s.namai.2012@gmail.com>
2014-01-17 09:14:50 +0900

    refactor

--- lib/Data/Sequential/Base.pm
+++ lib/Data/Sequential/Base.pm
@@ -4,15 +4,20 @@ use warnings;
 
 sub new {
     my ($class, $initial) = @_;
+    my $default = $class->initial_value;
     return bless {
-        initial => $initial // 0,
-        current => $initial // 0,
+        initial => $initial // $default,
+        current => $initial // $default,
     }, $class;
 }
 
 sub initial {
     my $self = shift;
     return $self->{initial};
+}
+
+sub initial_value {
+    die 'override me';
 }
 
 sub next {
